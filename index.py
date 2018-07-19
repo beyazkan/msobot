f34eb6f7fbf011b5a02b6ccf3149939566869706
beyazkan <iyimser.tuzlali@hotmail.com>
2018-07-19 21:44:19 +0300

    Düzeltme

--- index.py
+++ index.py
@@ -155,7 +155,7 @@ async def bot_banlayan(ctx):
     output = ''
     for user in bot_banlayanlar:
         output += user[1] + ' - ' + user[2] + ',\n'
-        output += "Lütfen bu kişiler ile görüşme sağlayıp, banlarını kaldırmalarını rica edin."
+    output += "Lütfen bu kişiler ile görüşme sağlayıp, banlarını kaldırmalarını rica edin."
 
     await client.say(output)
 
