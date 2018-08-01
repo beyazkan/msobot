e6a2c94e3b8d31ea931e021ed7768531467d859f
beyazkan <iyimser.tuzlali@hotmail.com>
2018-08-01 18:38:36 +0300

    Bot Lvl Sistemi Kaldırıldı.

--- index.py
+++ index.py
@@ -109,16 +109,6 @@ async def clear(ctx, amount=100):
 async def logout():
     await client.logout()
 
-@client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-    await update_data(users, member)
-
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
 @client.command(pass_context=True) # Here we are getting the member object
 async def duyuru(ctx, *args):
     server = ctx.message.server
@@ -158,39 +148,6 @@ async def bot_banlayan(ctx):
     output += "Lütfen bu kişiler ile görüşme sağlayıp, banlarını kaldırmalarını rica edin."
 
     await client.say(output)
-
-@client.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message.channel)
-
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-    await client.process_commands(message)
-
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 1
-
-async def add_experience(users, user, exp):
-    users[user.id]['experience'] += exp
-
-async def level_up(users, user, channel):
-    experience = users[user.id]['experience']
-    lvl_start = users[user.id]['level']
-    lvl_end = int(experience ** (1/4))
-
-    if lvl_start < lvl_end:
-        await client.send_message(channel, '{} adlı üyemiz, seviye {} oldu. Tebrikler...'.format(user.mention,lvl_end))
-        users[user.id]['level'] = lvl_end
 
 @client.command()
 async def load(extension):
